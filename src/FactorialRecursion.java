4e63893b6e4711510754705a627e2b2f9443c62a
Bhanu Sharma <bhanusharma.a3@gmail.com>
2021-09-22 19:36:06 +0530

    factorial using recusrion

--- src/FactorialRecursion.java
+++ src/FactorialRecursion.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class FactorialRecursion {
+public class FactorialRecursion {
+
+    private static int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+
+        return fact(n - 1) * n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(3));
+    }
 }
