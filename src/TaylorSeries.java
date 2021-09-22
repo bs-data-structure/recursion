f586630909cfd932388498cd4b3af2ae200131ec
Bhanu Sharma <bhanusharma.a3@gmail.com>
2021-09-22 20:51:20 +0530

    taylor series that uses n multiplication

--- src/TaylorSeries.java
+++ src/TaylorSeries.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class TaylorSeries {
+public class TaylorSeries {
+
+    private static double power = 1;
+    private static double fact = 1;
+    private static double factN = 1;
+
+    private static double taylorSeries(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        power *= x;
+        fact *= factN++;
+        double term = power / fact;
+
+        return taylorSeries(x, n - 1) + term;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(taylorSeries(3, 10));
+    }
 }
